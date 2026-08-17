ed7fbf430cb6249a4c93b2c7b99dc6b7aa05061f
sabarianandsabarianand45-cell <sabarianandsabarianand45@gmail.com>
2026-08-17 12:05:08 +0530

    Update solution.sql

--- Assignment04_Create_Course/solution.sql
+++ Assignment04_Create_Course/solution.sql
@@ -1,5 +1,11 @@
--- Create Course table
-
--- Insert three records
-
--- Display structure
+use cOllege;
+CREATE TABLE Course(
+CourseName VARCHAR (100),
+Credits INT,
+DepartmentID INT
+);
+INSERT INTO Course (CourseID ,CourseName,Credits,DepartmentID)
+VALUES(101,"Datebase Management System",4,1),
+(102,"Operating System",3,2),
+(103,"computer Networks",4,3);
+DESCRIBE COURSE;
