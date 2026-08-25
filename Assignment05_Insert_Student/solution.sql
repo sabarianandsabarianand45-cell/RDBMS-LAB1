f7d093bd95a558ffdd8b4ee0bc336c1c9a532054
sabarianandsabarianand45-cell <sabarianandsabarianand45@gmail.com>
2026-08-25 09:35:51 +0530

    Update solution.sql

--- Assignment05_Insert_Student/solution.sql
+++ Assignment05_Insert_Student/solution.sql
@@ -1,7 +1,23 @@
-INSERT INTO Student (StudentID, StudentName, Gender, DepartmentID)
-VALUES 
-    (1001, 'Arun', 'Male', 101),
-    (1002, 'Divya', 'Female', 102),
-    (1003, 'Karthik', 'Male', 101);
-    
+DROP DATABASE IF EXISTS CollegeDB;
+CREATE DATABASE CollegeDB;
+USE CollegeDB;
+
+CREATE TABLE Student(
+    StudentID INT(5) PRIMARY KEY,
+    StudentName VARCHAR(20) NOT NULL,
+    DOB DATE,
+    Gender VARCHAR(10),
+    DepartmentID INT(5),
+    Email VARCHAR(30),
+    PhoneNumber BIGINT
+);
+
+INSERT INTO Student(StudentID, StudentName, Gender, DepartmentID)
+VALUES (1001, 'Arun', 'Male', 101);
+
+INSERT INTO Student(StudentID, StudentName, Gender, DepartmentID)
+VALUES (1002, 'Divya', 'Female', 102);
+
+INSERT INTO Student(StudentID, StudentName, Gender, DepartmentID)
+VALUES (1003, 'Karthik', 'Male', 101);
 SELECT * FROM Student;
